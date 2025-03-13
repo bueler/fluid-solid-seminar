477da6f497af1e0c9494f308cedf320418afe391
Ed Bueler <elbueler@alaska.edu>
2025-03-13 10:39:00 -0800

    fiddle a bit more with demo ... good to go

--- py/bueler/deform.py
+++ py/bueler/deform.py
@@ -1,38 +1,44 @@
-# Generate a Paraview viewable 3D output file which allows visualization
+# Generate a Paraview-viewable 3D output file which allows visualization
 # of several maps as deformations of a cube.  These deformations Phi(x)
 # can be visualized using the "warp by vector" functionality of Paraview,
 # applied to the displacements u(x) = Phi(x) - x.
+# Demo:
+#   [start Firedrake venv]
+#   (firedrake) $ python3 deform.py       <-- writes result_deform.pvd
+#   (firedrake) $ paraview deform.pvsm
+# Then turn on one warp-by-vector view at a time.
 
 from firedrake import *
 
 mesh = UnitCubeMesh(10, 10, 10)
 x = SpatialCoordinate(mesh)
 
-# this function, used as coloring, will be helpful to show deformations
+# this function, used as coloring, will helps to show reference configuration
 F = FunctionSpace(mesh, 'CG', 1)
 f_ufl = x[0] * x[1] * x[2] + (1 - x[1]) ** 6.0
 f = Function(F, name='f').interpolate(f_ufl)
 
-# deformations and displacements in this space
+# deformations and displacements are vector functions in this space
 V = VectorFunctionSpace(mesh, 'CG', 1)
 
 # vertical reflection; not allowed in elasticity
 Phi1 = Function(V, name='Phi_1').interpolate(as_vector([x[0], x[1], - x[2]]))
-u1 = Function(V, name='u_1').interpolate(Phi1 - x)
+u1 = Function(V, name='u_1 (reflection)').interpolate(Phi1 - x)
 
-# rigid body rotation by 90 deg, fixing line x_2=0 & x_3=0
+# rigid body rotation by 90 deg, which fixes line x_2=0 & x_3=0
 Phi2 = Function(V, name='Phi_2').interpolate(as_vector([x[0], x[2], - x[1]]))
-u2 = Function(V, name='u_2').interpolate(Phi2 - x)
+u2 = Function(V, name='u_2 (90 deg rotation)').interpolate(Phi2 - x)
 
-# deformation which crushes x_2=0 face to a line; det(grad phi) -> 0 along line
+# deformation which crushes x_2=0 face to a line; det(grad phi) = 0 along x_2=0 face
 Phi3 = Function(V, name='Phi_3').interpolate(as_vector([x[0], x[1], x[1] * x[2]]))
-u3 = Function(V, name='u_3').interpolate(Phi3 - x)
+u3 = Function(V, name='u_3 (crush face)').interpolate(Phi3 - x)
 
-# twist deformation
+# stretch and twist deformation
 Phi4_ufl = [(1 - x[1]) * x[0] + x[1] * (- (x[2] - 0.5) + 0.5),
-            x[1],
+            2 * x[1],
             (1 - x[1]) * x[2] + x[1] * (x[0])]
 Phi4 = Function(V, name='Phi_4').interpolate(as_vector(Phi4_ufl))
-u4 = Function(V, name='u_4').interpolate(Phi4 - x)
+u4 = Function(V, name='u_4 (stretch and twist)').interpolate(Phi4 - x)
 
+# write coloring and all displacements
 VTKFile('result_deform.pvd').write(f, u1, u2, u3, u4)
